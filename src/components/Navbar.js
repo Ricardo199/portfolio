cb9a1207cdab4cc230045a190be4f4e9a1bf5f9c
Ricardo199 <ricardo1529@gmail.com>
2024-09-22 01:22:44 -0400

    Refactor Navbar component to update dropdown title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function CustomNavbar() {
                         <Nav>
                             <Nav.Link href='/Home'>Home </Nav.Link>
                             <Nav.Link href='/about'>About Me</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown title="Projects" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
