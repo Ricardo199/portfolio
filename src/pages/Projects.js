64da02fc28ae2331c2b959487c52b1a7c8150265
Ricardo199 <ricardo1529@gmail.com>
2024-09-25 22:35:34 -0400

    ```text Refactor Projects component to add a card for displaying a project with a GitHub link ```<|diff_marker|><|diff_marker|> --- git-commit-message/git-commit-message.py <|diff_marker|> 1000 +import difflib <|diff_marker|> --- git-commit-message/git-commit-message.py <|diff_marker|> 1001 +import re <|diff_marker|> --- git-commit-message/git-commit-message.py <|diff_marker|> 1002 +def remove_meta_info(commit_message): +    # Remove issue references +    commit_message = re.sub(r'#\d+', '', commit_message) +    # Remove tags +    commit_message = re.sub(r'\[.*?\]', '', commit_message) +    # Remove author names +    commit_message = re.sub(r'@.*?\b', '', commit_message) +    return commit_message.strip() <|diff_marker|> --- git-commit-message/git-commit-message.py <|diff_marker|> 1003 +def generate_commit_message(code_changes, recent_user_commits, recent_repository_commits): +    # Analyze code changes +    # TODO: Implement code changes analysis + +    # Identify purpose of changes +    # TODO: Implement purpose identification
    +    # Review recent repository commits
    +    # TODO: Implement review of recent repository commits
    
    +    # Generate commit message
    +    commit_message = "Refactor Projects component to add a card for displaying a project with a GitHub link"
    +
    +    # Remove meta information from commit message
    +    commit_message = remove_meta_info(commit_message)
    +
    +    return commit_message

---
+++ src/pages/Projects.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Card } from 'react-bootstrap';
+
+function Projects(){
+    <>
+        <div className="App bg-dark text-light">
+            <header className="App-header">
+                <Container>
+                    <h1 className="text-center">Projects</h1>
+                    <p>Here are some of the projects I have worked on:</p>
+                    <div className="d-flex justify-content-center">
+                        <Card style={{ width: '18rem' }} className="m-3">
+                            <Card.Img variant="top" src={"https://server2client.com/images/servletslogo.webp"} />
+                            <Card.Body>
+                                <Card.Title>Project 1</Card.Title>
+                                <Card.Text>
+                                    Java servlet app for managing an employee and their department to show off the functionality of the java servlet library
+                                    <a href="https://github.com/Ricardo199/sis16emp/tree/main">
+                                    Github link, because of certain limitations i am not able to run it but the code is here and functional
+                                    </a>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </Container>
+            </header>
+        </div>
+    </>
+}
+
+export default Projects;
